b2efe57a49d750adc44751167cbf2a6dbd3b8fb8
sayog <sayogkarki@gmail.com>
2020-05-07 18:35:33 +0530

    server config changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const server = new GraphQLServer({
   resolvers,
   resolverValidationOptions: {
     requireResolversForResolveType: false
-  }
+  },
   context: req => ({ ...req, models })
 });
 
